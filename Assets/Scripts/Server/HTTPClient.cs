9ab116263ab252db7e6832f7af90180da399071c
NVSao <saonguyenvan2003@gmail.com>
2023-07-26 11:04:58 +0700

    fix conflix

--- Assets/Scripts/Server/HTTPClient.cs
+++ Assets/Scripts/Server/HTTPClient.cs
@@ -1,32 +1,8 @@
 using Framework;
 using SimpleJSON;
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Security.Cryptography;
-using System.Text;
 using UnityEngine;
-using UnityEngine.Windows;
 
-public static class SHA256Hash
-{
-    public static string GetSHA256Hash(string input)
-    {
-        using (var sha256 = SHA256.Create())
-        {
-            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
-            byte[] hashBytes = sha256.ComputeHash(inputBytes);
-
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < hashBytes.Length; i++)
-            {
-                sb.Append(hashBytes[i].ToString("x2"));
-            }
-
-            return sb.ToString();
-        }
-    }
-}
 
 public class HTTPClient : SingletonMono<HTTPClient>    
 {
