aa3bf1ed02c04ea40c47511db5942aea112f87af
nvs <nvs@gmail.com>
2023-09-29 14:18:26 +0700

    update ui pve

---
+++ Assets/Scripts/PVE/UI/Popup/ShowRewardPopup.cs
@@ -0,0 +1,25 @@
+using Framework;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowRewardPopup : MonoBehaviour
+{
+    [SerializeField] Button forwardButton, backwardButton;
+    [SerializeField] TextMeshProUGUI amountReward;
+    [SerializeField] int rewardStartLevel;
+    //private PDataUnit<int> leveNeedShowReward = new PDataUnit<int>(rewardStartLevel);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    private void OnClickForwardButton()
+    {
+        
+    }
+}
