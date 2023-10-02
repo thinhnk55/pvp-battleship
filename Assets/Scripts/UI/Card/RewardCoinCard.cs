24ff4d9ce2618ad59f2bdfcfddbacea68b6a375e
LeThaiThinh18021228 <121384809+LeThaiThinh18021228@users.noreply.github.com>
2023-10-02 10:56:35 +0700

    fix royalpass

--- Assets/Scripts/UI/Card/RewardCoinCard.cs
+++ Assets/Scripts/UI/Card/RewardCoinCard.cs
@@ -1,29 +1,25 @@
 using Framework;
-using SimpleJSON;
 using System;
-using System.Collections.Generic;
 using TMPro;
-//using UnityEditor.PackageManager;
 using UnityEngine;
-using UnityEngine.UI;
 [Serializable]
 
 public struct RewardCoinInfo
 {
     public int Id;
-    public long Amount;    
+    public long Amount;
 }
 
 public class RewardCoinCard : CardBase<RewardCoinInfo>
-{   
+{
     [SerializeField] private TextMeshProUGUI amountTxt;
 
     public override void BuildUI(RewardCoinInfo info)
     {
         base.BuildUI(info);
 
-        amountTxt.text ="x"+ info.Amount.ToString();
-       
+        amountTxt.text = "x" + info.Amount.ToString();
+
     }
 
     protected override void OnClicked(RewardCoinInfo info)
