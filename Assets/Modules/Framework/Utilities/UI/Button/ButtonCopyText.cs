c124643892283e2b56d1b65c86673e960cabd223
LeThaiThinh18021228 <121384809+LeThaiThinh18021228@users.noreply.github.com>
2023-11-06 10:36:51 +0700

    butotn copy text

--- Assets/Modules/Framework/Utilities/UI/Button/ButtonCopyText.cs
+++ Assets/Modules/Framework/Utilities/UI/Button/ButtonCopyText.cs
@@ -8,7 +8,7 @@ namespace Framework
         protected override void Button_OnClicked()
         {
             base.Button_OnClicked();
-            GUIUtility.systemCopyBuffer = text.text;
+            GUIUtility.systemCopyBuffer = text.text.Substring(1);
         }
     }
 }
