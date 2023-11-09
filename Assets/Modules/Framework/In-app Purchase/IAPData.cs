fa6bff5f75b35328651d41d02fd055242e13ac92
LeThaiThinh18021228 <121384809+LeThaiThinh18021228@users.noreply.github.com>
2023-11-09 10:11:07 +0700

    fix IAPdata + disconnect popup transition

--- Assets/Modules/Framework/In-app Purchase/IAPData.cs
+++ Assets/Modules/Framework/In-app Purchase/IAPData.cs
@@ -7,5 +7,10 @@ namespace Framework
     public class IAPData : PDataBlock<IAPData>
     {
         Dictionary<Product, JSONNode> pendingProducts; public static Dictionary<Product, JSONNode> PendingProducts { get => Instance.pendingProducts; set => Instance.pendingProducts = value; }
+        protected override void Init()
+        {
+            base.Init();
+            PendingProducts ??= new();
+        }
     }
 }
